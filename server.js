5653b58d3e4ec3a250353f5aad45b2bd41eb612d
Galaxy092 <096polideth@gmail.com>
2022-07-05 12:37:13 +0700

    fix

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(db.users) })
+app.get('/', (req, res) => { res.send("it's working") })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 
